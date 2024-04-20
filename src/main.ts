6e6a809af02707d92d226abe8d4334cd185ce3ba
melanierebecca <melaniewijesooriya@gmail.com>
2024-04-20 09:25:07 +0400

    initial commit
    setup nest js project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Coin Transaction REST API')
+    .setDescription('Get stablecoin balance, transaction history, and simulate sending stablecoin to other addresses')
+    .setVersion('1.0')
+    .addTag('coins')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT);
+}
+bootstrap();
